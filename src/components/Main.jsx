9cb952400692afd3388eb66140177acfd528ac6e
j-grosse <pixelpause@gmx.de>
2023-07-27 12:48:50 +0200

    vite init

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <main>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default Main;
